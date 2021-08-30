e422a08f42c88283300cb81b6dd7d14ce90135b5
Kazuroni <sayajinbr30@gmail.com>
2021-08-30 02:01:36 -0300

    Final

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace aula_entrada
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string NomeDoUsuario;
+            string SobreNome;
+            string NomeCompleto;
+
+            Console.Write("Por favor, digite seu nome: ");
+            NomeDoUsuario = Console.ReadLine();
+            
+            Console.Write("agora seu sobrenome: ");
+            SobreNome = Console.ReadLine();
+
+            NomeCompleto = $"{NomeDoUsuario} {SobreNome}";
+            
+            Console.WriteLine($"Obrigado, {NomeCompleto}! Presione uma tecla para continuar...");
+            Console.ReadKey();
+            Console.WriteLine($"Nome de catalogo...: {SobreNome.ToUpper()} {NomeDoUsuario}");
+            Console.WriteLine("enter para finalizar o programa!");
+            Console.ReadKey();
+        }
+    }
+}
